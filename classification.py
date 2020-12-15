900d8c20511f60f365a780403b878ea3809d6480
Dion <dionhs@hotmail.co.uk>
2020-12-15 07:54:04 +0000

    add emg features, save features to file

--- classification.py
+++ classification.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pathlib import Path
 from argparse import ArgumentParser
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import KFold
@@ -6,8 +7,10 @@ from scipy.ndimage.filters import uniform_filter1d
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.metrics import balanced_accuracy_score, classification_report
 from sklearn.svm import SVC
+from sklearn.decomposition import PCA
 from pystruct.learners import FrankWolfeSSVM
 from pystruct.models import ChainCRF
+from pystruct.models import LatentGraphCRF
 from load_data import load_data
 from features import power_features, PLF, onset_features
 
@@ -21,34 +24,67 @@ args = parser.parse_args()
 #DATAFRAME
 x_train, y_train, x_test = load_data()
 
-def preprocess(x_raw, y, x_test_raw, k_features=None):
-    #FEATURE EXTRACTION
+def extract_features(x_raw, x_test_raw):
     print('extracting features...')
-    pf = power_features(x_raw[:, :2])
-    pf_test = power_features(x_test_raw[:, :2])
+    PF_path_train = Path('data', 'train_pf.csv')
+    PF_path_test = Path('data', 'test_pf.csv')
+    if PF_path_train.exists():
+        pf = np.loadtxt(PF_path_train, delimiter=',')
+    else:
+        pf = power_features(x_raw[:, :2])
+        np.savetxt(X=pf, fname=PF_path_train, delimiter=',')
+    if PF_path_test.exists():
+        pf_test = np.loadtxt(PF_path_test, delimiter=',')
+    else:
+        pf_test = power_features(x_test_raw[:, :2])
+        np.savetxt(X=pf_test, fname=PF_path_test, delimiter=',')
 
-    plf = PLF(x_raw[:, :2]).reshape(-1, 1)
-    plf_test = PLF(x_test_raw[:, :2]).reshape(-1, 1)
+    PLF_path_train = Path('data', 'train_plf.csv')
+    PLF_path_test = Path('data', 'test_plf.csv')
+    if PLF_path_train.exists():
+        plf = np.loadtxt(PLF_path_train, delimiter=',')
+    else:
+        plf = PLF(x_raw[:, :2])
+        np.savetxt(X=plf, fname=PLF_path_train, delimiter=',')
+    if PLF_path_test.exists():
+        plf_test = np.loadtxt(PLF_path_test, delimiter=',')
+    else:
+        plf_test = PLF(x_test_raw[:, :2])
+        np.savetxt(X=plf_test, fname=PLF_path_test, delimiter=',')
 
-    f_train = np.hstack((pf, plf))
-    f_test = np.hstack((pf_test, plf_test))
+    onsets = onset_features(x_raw[:,2]).reshape(-1,1)
+    onsets_test = onset_features(x_test_raw[:,2]).reshape(-1,1)
 
-    #SMOOTHING
-    #n = 7
-    #f_train = uniform_filter1d(f_train, axis=0, size=n, mode='nearest')
-    #f_test = uniform_filter1d(f_test, axis=0, size=n, mode='nearest')
+    f_train = np.hstack((pf, plf, onsets))
+    f_test = np.hstack((pf_test, plf_test, onsets_test))
+
+    return f_train, f_test
+
+def preprocess(f_train, y, f_test, k_features=None):
 
     #SCALING
     scaler = StandardScaler().fit(f_train)
     f_train = scaler.transform(f_train)
     f_test = scaler.transform(f_test)
 
+    #SMOOTHING
+    n = 7
+    f_train = uniform_filter1d(f_train, axis=0, size=n, mode='nearest')
+    f_test = uniform_filter1d(f_test, axis=0, size=n, mode='nearest')
+
+    '''
+    pca = PCA(n_components=5)
+    pca.fit(f_train[:, :10])
+    f_train = np.hstack((pca.transform(f_train[:, :10]), f_train[:,10:]))
+    f_test = np.hstack((pca.transform(f_test[:, :10]), f_test[:,10:]))
+    '''
+
     #FEATURE SELECTION
     #x, x_test = kBest(x, y, x_test, f_classif, k_features)
 
     return f_train, f_test
 
-def CV_score(x, y, model, k_features=None, report=False):
+def CV_score(x, y, model, C, gamma, k_features=None, report=False):
     kf = KFold(n_splits=N_SPLITS, shuffle=False)
     total_score = 0
     y_pred_all = np.zeros(y.size, dtype=int)
@@ -56,30 +92,40 @@ def CV_score(x, y, model, k_features=None, report=False):
         x_train_kf, x_test_kf = preprocess(
             x[train_idx], y[train_idx], x[test_idx], k_features)
 
-        # Reshape for CRF
-        x_train_kf = x_train_kf.reshape(2, -1, x_train_kf.shape[-1])
-        x_test_kf = x_test_kf.reshape(1, -1, x_test_kf.shape[-1])
-        y_train_kf = y[train_idx].reshape(2,-1)
-        y_test_kf = y[test_idx].reshape(1,-1)
-
         print('Training model')
-        model.fit(x_train_kf, y_train_kf)
-        y_pred = model.predict(x_test_kf)[0]
+        y_pred = model(x_train_kf, y[train_idx], x_test_kf, C, gamma)
         y_pred_all[test_idx] = y_pred
         total_score += balanced_accuracy_score(y[test_idx], y_pred)
     if report:
         print(classification_report(y, y_pred_all))
     return total_score/N_SPLITS
 
+def Chain_CRF(x, y, x_test):
+    # Reshape for CRF
+    #svc = SVC(class_weight='balanced', kernel='rbf', decision_function_shape='ovr')
+    #svc.fit(x, y)
+    #x = svc.decision_function(x)
+    #x_test = svc.decision_function(x_test)
+    #scaler = StandardScaler().fit(x)
+    #x = scaler.transform(x)
+    #x_test = scaler.transform(x_test)
+    x = x.reshape(-1, 21600, x.shape[-1])
+    x_test = x_test.reshape(-1, 21600, x.shape[-1])
+    y = y.reshape(-1, 21600)
+    crf = ChainCRF()
+    ssvm = FrankWolfeSSVM(model=crf, C=1, max_iter=10)
+    ssvm = NSlackSSVM(model=crf, C=0.1, max_iter=10)
+    ssvm.fit(x, y)
+    y_pred = np.array(ssvm.predict(x_test))
+    return y_pred.flatten()
+
+def SVM(x, y, x_test, C, gamma):
+    svc = SVC(class_weight='balanced', kernel='rbf', C=C, gamma=gamma)
+    svc.fit(x, y)
+    return svc.predict(x_test)
+
 if args.test:
     x_train, x_test = preprocess(x_train, y_train, x_test)
-    # Reshape for CRF
-    x_train = x_train.reshape(3, -1, x_train.shape[-1])
-    x_test = x_test.reshape(2, -1, x_test.shape[-1])
-    y_train = y_train.reshape(3,-1)
-    crf = ChainCRF()
-    model = FrankWolfeSSVM(model=crf, C=0.1, max_iter=10)
-    model.fit(x_train, y_train)
     y_pred = np.array(model.predict(x_test)).flatten()
 
     y_pred_ids = np.hstack((
@@ -90,13 +136,16 @@ if args.test:
             fmt=['%d', '%d'], comments='')
 
 elif args.finetune:
+    f_train, f_test = extract_features(x_train, x_test)
     scores = []
-    np.savetxt(fname='finetuning.csv', header='', delimiter=',',
-            X=scores, fmt=[], comments='')
+    for C in [0.1, 0.5, 1, 5, 10, 20]:
+        for gamma in ['auto', 'scale', 0.1, 0.01]:
+            model = SVM
+            scores.append(CV_score(f_train, y_train, model, C, gamma))
+    np.savetxt(fname='finetuning.csv', header='C,gamma', delimiter=',',
+            X=scores, fmt=['%.1f', '%.3f'], comments='')
 else:
-    #model = SVC(class_weight='balanced', kernel='rbf')
-    crf = ChainCRF()
-    model = FrankWolfeSSVM(model=crf, C=0.1, max_iter=10)
-    avg_score = CV_score(x_train, y_train, model)
+    model = SVM
+    f_train, f_test = extract_features(x_train, x_test)
+    avg_score = CV_score(f_train, y_train, model)
     print('Average BMAC:', avg_score)
-
--- features.py
+++ features.py
@@ -6,32 +6,35 @@ import biosppy.signals.emg as emg
 # Input is a N x 2 x 512 matrix
 # Output is a N x 10 matrix
 def power_features(x):
-    pf_all = np.zeros((x.shape[0], x.shape[1], 5))
+    x = x.reshape(-1, 21600, 2, 512)
+    pf_all = np.zeros((x.shape[0], 21600, 10))
     for i in range(x.shape[0]):
-        pf = eeg.get_power_features(x[i].transpose(), sampling_rate=128, size=4)
-        pf_all[i] = np.vstack(pf[1:]).transpose()
+        pf = eeg.get_power_features(x[i].transpose((0,2,1)).reshape(-1, 2), sampling_rate=128, size=4, overlap=0)
+        pf_all[i] = np.hstack(pf[1:])
     return pf_all.reshape(-1, 10)
 
 
 # Returns Phase-Locking Features (PLF)
 # Input is a N x 2 x 512 matrix
-# Output is a N-array
+# Output is a N x 2 matrix
 def PLF(x):
-    plf_all = np.zeros((x.shape[0]))
+    x = x.reshape(-1, 21600, 2, 512)
+    plf_all = np.zeros((x.shape[0], 21600, 2))
     for i in range(x.shape[0]):
-        ts, plf_par, plf = eeg.get_plf_features(x[i].transpose(), sampling_rate=128, size=4)
-        plf_all[i] = plf.item()
-    return plf_all
+        ts, plf_par, plf = eeg.get_plf_features(x[i].transpose((0,2,1)).reshape(-1,2),
+                sampling_rate=128, size=4, overlap=0)
+        plf_all[i] = plf
+    return plf_all.reshape(-1, 2)
 
 
-# Finds onset indices of EMG pulses
+# Finds number of onsets for each 4s window
 # Input is a N x 512 matrix
 # Output is a N array
-# Not working yet. emg.emg has problems with our signal
 def onset_features(x):
-    onset_all = []
+    x = x.reshape(-1, 21600 * 512)
+    onset_freq = np.zeros((x.shape[0], 21600))
     for i in range(x.shape[0]):
-        ts, filtered, onset_indices = emg.emg(x[i], sampling_rate=128, show=True)
-        onset_all.append(onset_indices)
-    return onset_indices.transpose()
+        onsets = emg.find_onsets(x[i], sampling_rate=128)[0]
+        onset_freq[i] = np.histogram(onsets, bins=21600)[0]
+    return onset_freq.flatten()
 
